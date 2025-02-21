a28a48bd5e9a319c4bca38582ab7f3819476997b
Zazaqui77 <sheeld679@gmail.com>
2025-02-21 16:29:50 +0000

    Salvar

--- exercicios.py/6.exercico.py
+++ exercicios.py/6.exercico.py
@@ -11,7 +11,7 @@ idade = int (input("Digite sua idade : "))
 
 if idade < 18 or idade > 65:
     print("É obrigado a votar!")
-if idade > 16 and idade < 17 or go65:
+if idade > 16 and idade < 17 or 65:
     print("Voto opcional")
 if idade > 18:
     print("Não é opicional")    
