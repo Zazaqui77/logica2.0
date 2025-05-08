49f89707d10f40189040feb7db29a81c70824ca5
Zazaqui77 <sheeld679@gmail.com>
2025-05-08 19:57:01 +0000

    Salvando

---
+++ Classes.py/classe_5.py
@@ -0,0 +1,16 @@
+import os 
+from dataclasses import dataclass
+os.system("Clas || clear")
+
+@dataclass
+class Pessoa:
+    nome: str
+    idade: int
+    altura: float
+
+class endereco:
+    logradouro: str
+    endereco: str
+    numero: int
+
+    
